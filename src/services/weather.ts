6e1cf472e97794669205e2f0cd27e4ee11e8c899
YaelAram <yaelaram14@outlook.com>
2023-09-13 15:54:35 -0600

    Se corrigio un error en el servicio de clima

--- src/services/weather.ts
+++ src/services/weather.ts
@@ -13,14 +13,9 @@ export const weather = async (lat: number, lon: number) => {
 
   const resp = await fetch(`${url}?${params.toString()}`);
   const weatherResp: WeatherResp = await resp.json();
-  const weatherDes = weatherResp.weather.at(0)?.description;
 
   return {
     temp: Math.round(weatherResp.main.temp),
     location: weatherResp.name,
-    weather: `${weatherDes?.at(0)?.toUpperCase()}${weatherDes?.slice(1)}`,
-    icon: `https://openweathermap.org/img/wn/${
-      weatherResp.weather.at(0)?.icon
-    }@2x.png`,
   };
 };
